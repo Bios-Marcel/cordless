5bdee16e68c986a1cc7da0e9a469c5a686b7b961
Marcel Schramm <marceloschr@googlemail.com>
2019-05-25 11:22:25 +0200

    Add tests for guild sorting depending on user settings.

---
+++ discordutil/guilds_test.go
@@ -0,0 +1,48 @@
+package discordutil
+
+import (
+	"testing"
+
+	"github.com/Bios-Marcel/discordgo"
+)
+
+func TestSortGuilds(t *testing.T) {
+	//The IDs have random length and don't mean much.
+	guildOneID := "98122541287"
+	guildTwoID := "12450501965"
+	guildThreeID := "1086518z963"
+	guildFourID := "19651241842"
+	settings := &discordgo.Settings{
+		GuildPositions: []string{
+			guildOneID,
+			guildTwoID,
+			guildThreeID,
+			guildFourID,
+		},
+	}
+
+	guilds := []*discordgo.UserGuild{
+		&discordgo.UserGuild{ID: guildThreeID},
+		&discordgo.UserGuild{ID: guildOneID},
+		&discordgo.UserGuild{ID: guildFourID},
+		&discordgo.UserGuild{ID: guildTwoID},
+	}
+
+	SortGuilds(settings, guilds)
+
+	if guilds[0].ID != guildOneID {
+		t.Errorf("The first guild should've been %s, but was %s", guildOneID, guilds[0].ID)
+	}
+
+	if guilds[1].ID != guildTwoID {
+		t.Errorf("The second guild should've been %s, but was %s", guildTwoID, guilds[1].ID)
+	}
+
+	if guilds[2].ID != guildThreeID {
+		t.Errorf("The third guild should've been %s, but was %s", guildThreeID, guilds[2].ID)
+	}
+
+	if guilds[3].ID != guildFourID {
+		t.Errorf("The fourth guild should've been %s, but was %s", guildFourID, guilds[3].ID)
+	}
+}
