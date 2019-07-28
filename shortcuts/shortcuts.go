d9958ee11532aa5e753da4bb272be4cd96e51157
Marcel Schramm <marceloschr@googlemail.com>
2019-07-28 17:10:56 +0200

    * Adding new shortcuts doesn't require manually adding them to the `Shortcuts` array anymore * The `defaultEvent` and the `Event` of a shortcut now initially point to the same object in memory

--- shortcuts/shortcuts.go
+++ shortcuts/shortcuts.go
@@ -13,236 +13,94 @@ import (
 )
 
 var (
-	globalScope = &Scope{
-		Parent:     nil,
-		Identifier: "global",
-		Name:       "Application wide",
-	}
+	globalScope        = addScope("Application wide", "global", nil)
+	multilineTextInput = addScope("Multiline text input", "multiline_text_input", globalScope)
 
-	multilineTextInput = &Scope{
-		Parent:     globalScope,
-		Identifier: "multiline_text_input",
-		Name:       "Multiline text input",
-	}
+	ExpandSelectionToLeft = addShortcut("expand_selection_word_to_left", "Expand selection word to left",
+		multilineTextInput, tcell.NewEventKey(tcell.KeyLeft, 0, tcell.ModShift))
+	ExpandSelectionToRight = addShortcut("expand_selection_word_to_right", "Expand selection word to right",
+		multilineTextInput, tcell.NewEventKey(tcell.KeyRight, 0, tcell.ModShift))
+	SelectAll = addShortcut("select_all", "Select all",
+		multilineTextInput, tcell.NewEventKey(tcell.KeyCtrlA, rune(tcell.KeyCtrlA), tcell.ModCtrl))
+	SelectWordLeft = addShortcut("select_word_to_left", "Select word to left",
+		multilineTextInput, tcell.NewEventKey(tcell.KeyLeft, 0, tcell.ModCtrl|tcell.ModShift))
+	SelectWordRight = addShortcut("select_word_to_right", "Select word to right",
+		multilineTextInput, tcell.NewEventKey(tcell.KeyRight, 0, tcell.ModCtrl|tcell.ModShift))
 
-	ExpandSelectionToLeft = &Shortcut{
-		Name:         "Expand selection word to left",
-		Identifier:   "expand_selection_word_to_left",
-		scope:        multilineTextInput,
-		Event:        tcell.NewEventKey(tcell.KeyLeft, 0, tcell.ModShift),
-		defaultEvent: tcell.NewEventKey(tcell.KeyLeft, 0, tcell.ModShift),
-	}
-	ExpandSelectionToRight = &Shortcut{
-		Name:         "Expand selection word to right",
-		Identifier:   "expand_selection_word_to_right",
-		scope:        multilineTextInput,
-		Event:        tcell.NewEventKey(tcell.KeyRight, 0, tcell.ModShift),
-		defaultEvent: tcell.NewEventKey(tcell.KeyRight, 0, tcell.ModShift),
-	}
-	SelectAll = &Shortcut{
-		Name:         "Select all",
-		Identifier:   "select_all",
-		scope:        multilineTextInput,
-		Event:        tcell.NewEventKey(tcell.KeyCtrlA, rune(tcell.KeyCtrlA), tcell.ModCtrl),
-		defaultEvent: tcell.NewEventKey(tcell.KeyCtrlA, rune(tcell.KeyCtrlA), tcell.ModCtrl),
-	}
-	SelectWordLeft = &Shortcut{
-		Name:         "Select word to left",
-		Identifier:   "select_word_to_left",
-		scope:        multilineTextInput,
-		Event:        tcell.NewEventKey(tcell.KeyLeft, 0, tcell.ModCtrl|tcell.ModShift),
-		defaultEvent: tcell.NewEventKey(tcell.KeyLeft, 0, tcell.ModCtrl|tcell.ModShift),
-	}
-
-	SelectWordRight = &Shortcut{
-		Name:         "Select word to right",
-		Identifier:   "select_word_to_right",
-		scope:        multilineTextInput,
-		Event:        tcell.NewEventKey(tcell.KeyRight, 0, tcell.ModCtrl|tcell.ModShift),
-		defaultEvent: tcell.NewEventKey(tcell.KeyRight, 0, tcell.ModCtrl|tcell.ModShift),
-	}
-
-	MoveCursorLeft = &Shortcut{
-		Name:         "Move cursor to left",
-		Identifier:   "move_cursor_to_left",
-		scope:        multilineTextInput,
-		Event:        tcell.NewEventKey(tcell.KeyLeft, 0, tcell.ModNone),
-		defaultEvent: tcell.NewEventKey(tcell.KeyLeft, 0, tcell.ModNone),
-	}
-	MoveCursorRight = &Shortcut{
-		Name:         "Move cursor to right",
-		Identifier:   "move_cursor_to_right",
-		scope:        multilineTextInput,
-		Event:        tcell.NewEventKey(tcell.KeyRight, 0, tcell.ModNone),
-		defaultEvent: tcell.NewEventKey(tcell.KeyRight, 0, tcell.ModNone),
-	}
-	MoveCursorWordLeft = &Shortcut{
-		Name:         "Move cursor to word left",
-		Identifier:   "move_cursor_to_word_left",
-		scope:        multilineTextInput,
-		Event:        tcell.NewEventKey(tcell.KeyLeft, 0, tcell.ModCtrl),
-		defaultEvent: tcell.NewEventKey(tcell.KeyLeft, 0, tcell.ModCtrl),
-	}
-	MoveCursorWordRight = &Shortcut{
-		Name:         "Move cursor to word right",
-		Identifier:   "move_cursor_to_word_right",
-		scope:        multilineTextInput,
-		Event:        tcell.NewEventKey(tcell.KeyRight, 0, tcell.ModCtrl),
-		defaultEvent: tcell.NewEventKey(tcell.KeyRight, 0, tcell.ModCtrl),
-	}
+	MoveCursorLeft = addShortcut("move_cursor_to_left", "Move cursor to left",
+		multilineTextInput, tcell.NewEventKey(tcell.KeyLeft, 0, tcell.ModNone))
+	MoveCursorRight = addShortcut("move_cursor_to_right", "Move cursor to right",
+		multilineTextInput, tcell.NewEventKey(tcell.KeyRight, 0, tcell.ModNone))
+	MoveCursorWordLeft = addShortcut("move_cursor_to_word_left", "Move cursor to word left",
+		multilineTextInput, tcell.NewEventKey(tcell.KeyLeft, 0, tcell.ModCtrl))
+	MoveCursorWordRight = addShortcut("move_cursor_to_word_right", "Move cursor to word right",
+		multilineTextInput, tcell.NewEventKey(tcell.KeyRight, 0, tcell.ModCtrl))
 
 	// FIXME Gotta add this later, as there is Backspace and Backspace and those differ on linux.
-	/*DeleteLeft = &Shortcut{
-		Name:         "Delete left",
-		Identifier:   "delete_left",
-		scope:        multilineTextInput,
-		Event:        tcell.NewEventKey(tcell.KeyBackspace2, rune(tcell.KeyBackspace2), tcell.ModNone),
-		defaultEvent: tcell.NewEventKey(tcell.KeyBackspace2, rune(tcell.KeyBackspace2), tcell.ModNone),
-	}*/
+	// DeleteLeft = addShortcut("delete_left","Delete left",multilineTextInput,tcell.NewEventKey(tcell.KeyBackspace2, rune(tcell.KeyBackspace2), tcell.ModNone))
 
-	DeleteRight = &Shortcut{
-		Name:         "Delete right",
-		Identifier:   "delete_right",
-		scope:        multilineTextInput,
-		Event:        tcell.NewEventKey(tcell.KeyDelete, 0, tcell.ModNone),
-		defaultEvent: tcell.NewEventKey(tcell.KeyDelete, 0, tcell.ModNone),
-	}
+	DeleteRight = addShortcut("delete_right", "Delete right",
+		multilineTextInput, tcell.NewEventKey(tcell.KeyDelete, 0, tcell.ModNone))
+	InputNewLine = addShortcut("add_new_line_character", "Add new line character",
+		multilineTextInput, tcell.NewEventKey(tcell.KeyEnter, rune(tcell.KeyEnter), tcell.ModAlt))
 
-	InputNewLine = &Shortcut{
-		Name:         "Add new line character",
-		Identifier:   "add_new_line_character",
-		scope:        multilineTextInput,
-		Event:        tcell.NewEventKey(tcell.KeyEnter, rune(tcell.KeyEnter), tcell.ModAlt),
-		defaultEvent: tcell.NewEventKey(tcell.KeyEnter, rune(tcell.KeyEnter), tcell.ModAlt),
-	}
+	ExitApplication = addShortcut("exit_application", "Exit application",
+		globalScope, tcell.NewEventKey(tcell.KeyCtrlC, rune(tcell.KeyCtrlC), tcell.ModCtrl))
 
-	ExitApplication = &Shortcut{
-		Name:         "Exit application",
-		Identifier:   "exit_application",
-		scope:        globalScope,
-		Event:        tcell.NewEventKey(tcell.KeyCtrlC, rune(tcell.KeyCtrlC), tcell.ModCtrl),
-		defaultEvent: tcell.NewEventKey(tcell.KeyCtrlC, rune(tcell.KeyCtrlC), tcell.ModCtrl),
-	}
+	FocusChannelContainer = addShortcut("focus_channel_container", "Focus channel container",
+		globalScope, tcell.NewEventKey(tcell.KeyRune, 'c', tcell.ModAlt))
+	FocusUserContainer = addShortcut("focus_user_container", "Focus user container",
+		globalScope, tcell.NewEventKey(tcell.KeyRune, 'u', tcell.ModAlt))
+	FocusGuildContainer = addShortcut("focus_guild_container", "Focus guild container",
+		globalScope, tcell.NewEventKey(tcell.KeyRune, 's', tcell.ModAlt))
+	FocusPrivateChatPage = addShortcut("focus_private_chat_page", "Focus private chat page",
+		globalScope, tcell.NewEventKey(tcell.KeyRune, 'p', tcell.ModAlt))
+	FocusMessageInput = addShortcut("focus_message_input", "Focus message input",
+		globalScope, tcell.NewEventKey(tcell.KeyRune, 'm', tcell.ModAlt))
+	FocusMessageContainer = addShortcut("focus_message_container", "Focus message container",
+		globalScope, tcell.NewEventKey(tcell.KeyRune, 't', tcell.ModAlt))
+	FocusCommandInput = addShortcut("focus_command_input", "Focus command input",
+		globalScope, tcell.NewEventKey(tcell.KeyCtrlI, rune(tcell.KeyCtrlI), tcell.ModNone))
+	FocusCommandOutput = addShortcut("focus_command_output", "Focus command output",
+		globalScope, tcell.NewEventKey(tcell.KeyCtrlO, rune(tcell.KeyCtrlO), tcell.ModCtrl))
 
-	FocusChannelContainer = &Shortcut{
-		Name:         "Focus channel container",
-		Identifier:   "focus_channel_container",
-		scope:        globalScope,
-		Event:        tcell.NewEventKey(tcell.KeyRune, 'c', tcell.ModAlt),
-		defaultEvent: tcell.NewEventKey(tcell.KeyRune, 'c', tcell.ModAlt),
-	}
+	ToggleUserContainer = addShortcut("toggle_user_container", "Toggle user container",
+		globalScope, tcell.NewEventKey(tcell.KeyRune, 'U', tcell.ModAlt))
+	ToggleCommandView = addShortcut("toggle_command_view", "Toggle command view",
+		globalScope, tcell.NewEventKey(tcell.KeyRune, '.', tcell.ModAlt))
 
-	FocusUserContainer = &Shortcut{
-		Name:         "Focus user container",
-		Identifier:   "focus_user_container",
-		scope:        globalScope,
-		Event:        tcell.NewEventKey(tcell.KeyRune, 'u', tcell.ModAlt),
-		defaultEvent: tcell.NewEventKey(tcell.KeyRune, 'u', tcell.ModAlt),
-	}
+	SendMessage = addShortcut("send_message", "Sends the typed message",
+		multilineTextInput, tcell.NewEventKey(tcell.KeyEnter, rune(tcell.KeyEnter), tcell.ModNone))
 
-	ToggleUserContainer = &Shortcut{
-		Name:         "Toggle user container",
-		Identifier:   "toggle_user_container",
-		scope:        globalScope,
-		Event:        tcell.NewEventKey(tcell.KeyRune, 'U', tcell.ModAlt),
-		defaultEvent: tcell.NewEventKey(tcell.KeyRune, 'U', tcell.ModAlt),
-	}
-
-	FocusGuildContainer = &Shortcut{
-		Name:         "Focus guild container",
-		Identifier:   "focus_guild_container",
-		scope:        globalScope,
-		Event:        tcell.NewEventKey(tcell.KeyRune, 's', tcell.ModAlt),
-		defaultEvent: tcell.NewEventKey(tcell.KeyRune, 's', tcell.ModAlt),
-	}
-
-	FocusPrivateChatPage = &Shortcut{
-		Name:         "Focus private chat page",
-		Identifier:   "focus_private_chat_page",
-		scope:        globalScope,
-		Event:        tcell.NewEventKey(tcell.KeyRune, 'p', tcell.ModAlt),
-		defaultEvent: tcell.NewEventKey(tcell.KeyRune, 'p', tcell.ModAlt),
-	}
-
-	FocusMessageInput = &Shortcut{
-		Name:         "Focus message input",
-		Identifier:   "focus_message_input",
-		scope:        globalScope,
-		Event:        tcell.NewEventKey(tcell.KeyRune, 'm', tcell.ModAlt),
-		defaultEvent: tcell.NewEventKey(tcell.KeyRune, 'm', tcell.ModAlt),
-	}
-
-	FocusMessageContainer = &Shortcut{
-		Name:         "Focus message container",
-		Identifier:   "focus_message_container",
-		scope:        globalScope,
-		Event:        tcell.NewEventKey(tcell.KeyRune, 't', tcell.ModAlt),
-		defaultEvent: tcell.NewEventKey(tcell.KeyRune, 't', tcell.ModAlt),
-	}
-
-	FocusCommandInput = &Shortcut{
-		Name:         "Focus command input",
-		Identifier:   "focus_command_input",
-		scope:        globalScope,
-		Event:        tcell.NewEventKey(tcell.KeyCtrlI, rune(tcell.KeyCtrlI), tcell.ModNone),
-		defaultEvent: tcell.NewEventKey(tcell.KeyCtrlI, rune(tcell.KeyCtrlI), tcell.ModNone),
-	}
-
-	FocusCommandOutput = &Shortcut{
-		Name:         "Focus command output",
-		Identifier:   "focus_command_output",
-		scope:        globalScope,
-		Event:        tcell.NewEventKey(tcell.KeyCtrlO, rune(tcell.KeyCtrlO), tcell.ModCtrl),
-		defaultEvent: tcell.NewEventKey(tcell.KeyCtrlO, rune(tcell.KeyCtrlO), tcell.ModCtrl),
-	}
-
-	ToggleCommandView = &Shortcut{
-		Name:         "Toggle command view",
-		Identifier:   "toggle_command_view",
-		scope:        globalScope,
-		Event:        tcell.NewEventKey(tcell.KeyRune, '.', tcell.ModAlt),
-		defaultEvent: tcell.NewEventKey(tcell.KeyRune, '.', tcell.ModAlt),
-	}
-
-	SendMessage = &Shortcut{
-		Name:         "Sends the typed message",
-		Identifier:   "send_message",
-		scope:        multilineTextInput,
-		Event:        tcell.NewEventKey(tcell.KeyEnter, rune(tcell.KeyEnter), tcell.ModNone),
-		defaultEvent: tcell.NewEventKey(tcell.KeyEnter, rune(tcell.KeyEnter), tcell.ModNone),
-	}
-
-	scopes = []*Scope{
-		globalScope,
-		multilineTextInput,
-	}
-
-	Shortcuts = []*Shortcut{
-		ExitApplication,
-		FocusChannelContainer,
-		FocusUserContainer,
-		ToggleUserContainer,
-		FocusGuildContainer,
-		FocusPrivateChatPage,
-		FocusMessageInput,
-		FocusMessageContainer,
-		FocusCommandInput,
-		FocusCommandOutput,
-		ToggleCommandView,
-		InputNewLine,
-		MoveCursorLeft,
-		MoveCursorRight,
-		MoveCursorWordLeft,
-		MoveCursorWordRight,
-		ExpandSelectionToLeft,
-		ExpandSelectionToRight,
-		SelectAll,
-		SelectWordLeft,
-		SelectWordRight,
-		DeleteRight,
-		SendMessage,
-	}
+	scopes    []*Scope
+	Shortcuts []*Shortcut
 )
+
+func addScope(name, identifier string, parent *Scope) *Scope {
+	scope := &Scope{
+		Parent:     parent,
+		Identifier: identifier,
+		Name:       name,
+	}
+
+	scopes = append(scopes, scope)
+
+	return scope
+}
+
+func addShortcut(name, identifier string, scope *Scope, event *tcell.EventKey) *Shortcut {
+	shortcut := &Shortcut{
+		Identifier:   identifier,
+		Name:         name,
+		scope:        scope,
+		Event:        event,
+		defaultEvent: event,
+	}
+
+	Shortcuts = append(Shortcuts, shortcut)
+
+	return shortcut
+}
 
 // Shortcut defines a shortcut within the application. The scope might for
 // example be a widget or situation in which the user is.
