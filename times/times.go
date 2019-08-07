3e4697963918d57bfd1dfb456bf18e4f013e2862
Avahe Kellenberger <avahe@protonmail.ch>
2019-08-07 08:43:11 -0400

    All quotes now use UTC time.

--- times/times.go
+++ times/times.go
@@ -7,19 +7,22 @@ import (
 	"github.com/Bios-Marcel/cordless/config"
 )
 
-// TimeToString formats a time to a string depending on the users settings.
+// TimeToLocalString formats a time to a string depending on the users settings.
 // The time will first be converted into a local time.
+func TimeToLocalString(time *time.Time) string {
+	localTime := time.Local()
+	return TimeToString(&localTime)
+}
+
+// TimeToString formats a time to a string depending on the users settings.
 func TimeToString(time *time.Time) string {
 	if config.GetConfig().Times == config.NoTime {
 		return ""
 	}
-
-	localTime := time.Local()
 	if config.GetConfig().Times == config.HourMinuteAndSeconds {
-		return fmt.Sprintf("%02d:%02d:%02d", localTime.Hour(), localTime.Minute(), localTime.Second())
+		return fmt.Sprintf("%02d:%02d:%02d", time.Hour(), time.Minute(), time.Second())
 	} else if config.GetConfig().Times == config.HourAndMinute {
-		return fmt.Sprintf("%02d:%02d", localTime.Hour(), localTime.Minute())
+		return fmt.Sprintf("%02d:%02d", time.Hour(), time.Minute())
 	}
-
 	return ""
 }
