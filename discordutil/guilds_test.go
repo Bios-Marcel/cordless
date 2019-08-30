ed8690865520a27a8df13be195a186d502927f40
Marcel Schramm <marceloschr@googlemail.com>
2019-08-30 23:18:16 +0200

    LoadGuilds from the guild util class is now tested using a mocked guildLoader

--- discordutil/guilds_test.go
+++ discordutil/guilds_test.go
@@ -1,6 +1,9 @@
 package discordutil
 
 import (
+	"errors"
+	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/Bios-Marcel/discordgo"
@@ -44,5 +47,125 @@ func TestSortGuilds(t *testing.T) {
 
 	if guilds[3].ID != guildFourID {
 		t.Errorf("The fourth guild should've been %s, but was %s", guildFourID, guilds[3].ID)
+	}
+}
+
+type testGuildLoader struct {
+	loadFunction func(int, string, string) ([]*discordgo.UserGuild, error)
+}
+
+func (loader testGuildLoader) UserGuilds(amount int, beforeID, afterID string) ([]*discordgo.UserGuild, error) {
+	return loader.loadFunction(amount, beforeID, afterID)
+}
+
+func generateGuilds(start, amount int) []*discordgo.UserGuild {
+	guilds := make([]*discordgo.UserGuild, 0, amount)
+	for i := start; i < start+amount; i++ {
+		fmt.Println(i)
+		guilds = append(guilds, &discordgo.UserGuild{ID: fmt.Sprintf("%d", i)})
+	}
+
+	return guilds
+}
+
+func TestLoadGuilds(t *testing.T) {
+	type args struct {
+		guildLoader GuildLoader
+	}
+	tests := []struct {
+		name        string
+		guildLoader GuildLoader
+		want        []*discordgo.UserGuild
+		wantErr     bool
+	}{
+		{
+			name: "forward error",
+			guildLoader: testGuildLoader{func(amount int, beforeID, afterID string) ([]*discordgo.UserGuild, error) {
+				return nil, errors.New("owo, an error")
+			}},
+			want:    nil,
+			wantErr: true,
+		}, {
+			name: "no guilds",
+			guildLoader: testGuildLoader{func(amount int, beforeID, afterID string) ([]*discordgo.UserGuild, error) {
+				return nil, nil
+			}},
+			want:    []*discordgo.UserGuild{},
+			wantErr: false,
+		}, {
+			name: "100 guilds",
+			guildLoader: testGuildLoader{func(amount int, beforeID, afterID string) ([]*discordgo.UserGuild, error) {
+				//100 is the API limit
+				if amount == 100 {
+					if beforeID == "" {
+						return generateGuilds(1, 100), nil
+					} else if beforeID == "1" {
+						return []*discordgo.UserGuild{}, nil
+					}
+
+					return nil, errors.New("unsupported case")
+				}
+
+				return nil, errors.New("test only supports usecase of 100 at once")
+			}},
+			want:    generateGuilds(1, 100),
+			wantErr: false,
+		}, {
+			name: "150 guilds",
+			guildLoader: testGuildLoader{func(amount int, beforeID, afterID string) ([]*discordgo.UserGuild, error) {
+				//100 is the API limit
+				if amount == 100 {
+					if beforeID == "" {
+						return generateGuilds(51, 100), nil
+					} else if beforeID == "51" {
+						return generateGuilds(1, 50), nil
+					}
+
+					return nil, errors.New("unsupported case")
+				}
+
+				return nil, errors.New("test only supports usecase of 100 at once")
+			}},
+			want:    generateGuilds(1, 150),
+			wantErr: false,
+		}, {
+			name: "200 guilds",
+			guildLoader: testGuildLoader{func(amount int, beforeID, afterID string) ([]*discordgo.UserGuild, error) {
+				//100 is the API limit
+				if amount == 100 {
+					if beforeID == "" {
+						return generateGuilds(101, 100), nil
+					} else if beforeID == "101" {
+						return generateGuilds(1, 100), nil
+					} else if beforeID == "1" {
+						return []*discordgo.UserGuild{}, nil
+					}
+
+					return nil, errors.New("unsupported case")
+				}
+
+				return nil, errors.New("test only supports usecase of 100 at once")
+			}},
+			want:    generateGuilds(1, 200),
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := LoadGuilds(tt.guildLoader)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("LoadGuilds() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if len(got) != len(tt.want) {
+				t.Errorf("length of LoadGuilds() = %v, want %v", len(got), len(tt.want))
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("LoadGuilds() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
