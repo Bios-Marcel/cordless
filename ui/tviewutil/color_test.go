6955fcff89e2eb241f27398c567fd8296c7424ec
Marcel Schramm <marceloschr@googlemail.com>
2019-09-02 23:38:58 +0200

    Color tests now use rgb colors, instead of predefined ones.

--- ui/tviewutil/color_test.go
+++ ui/tviewutil/color_test.go
@@ -15,23 +15,23 @@ func TestColorToHex(t *testing.T) {
 	}{
 		{
 			name:  "black",
-			color: tcell.ColorBlack,
+			color: tcell.NewRGBColor(0, 0, 0),
 			want:  "#000000",
 		}, {
 			name:  "white",
-			color: tcell.ColorWhite,
+			color: tcell.NewRGBColor(255, 255, 255),
 			want:  "#ffffff",
 		}, {
 			name:  "red",
-			color: tcell.ColorRed,
+			color: tcell.NewRGBColor(255, 0, 0),
 			want:  "#ff0000",
 		}, {
 			name:  "green",
-			color: tcell.ColorRed,
+			color: tcell.NewRGBColor(0, 255, 0),
 			want:  "#00ff00",
 		}, {
 			name:  "blue",
-			color: tcell.ColorRed,
+			color: tcell.NewRGBColor(0, 0, 255),
 			want:  "#0000ff",
 		},
 	}
