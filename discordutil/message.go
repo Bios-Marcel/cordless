f8e74cfba90a57026718fe1c525d689f098e22dc
Marcel Schramm <marceloschr@googlemail.com>
2019-10-06 18:07:50 +0200

    Add docs for message.go [skip ci]

--- discordutil/message.go
+++ discordutil/message.go
@@ -2,6 +2,8 @@ package discordutil
 
 import "github.com/Bios-Marcel/discordgo"
 
+// MentionsCurrentUserExplicitly checks whether the message contains any
+// explicit mentions for the user associated with the currently logged in user.
 func MentionsCurrentUserExplicitly(state *discordgo.State, message *discordgo.Message) bool {
 	for _, user := range message.Mentions {
 		if user.ID == state.User.ID {
