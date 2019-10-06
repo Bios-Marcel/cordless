9fb71f91c2b2c41e7c71ff2cfeb4593e69742a4f
Marcel Schramm <marceloschr@googlemail.com>
2019-10-06 14:39:45 +0200

    Format code and remove unreachable break statement

--- discordutil/message.go
+++ discordutil/message.go
@@ -5,8 +5,7 @@ import "github.com/Bios-Marcel/discordgo"
 func MentionsCurrentUserExplicitly(state *discordgo.State, message *discordgo.Message) bool {
 	for _, user := range message.Mentions {
 		if user.ID == state.User.ID {
-				return true
-			break
+			return true
 		}
 	}
 
