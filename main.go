9dc91ce4b07bd515aabb02fdf07dc60352a5bcbc
Aidan Culley <legoguy253@gmail.com>
2019-11-04 16:08:15 -0600

    implementing frontend to config flag

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 func main() {
 	showVersion := flag.Bool("version", false, "Show the version instead of starting cordless")
 	showShortcutsDialog := flag.Bool("shortcut-dialog", false, "Shows the shortcuts dialog instead of launching cordless")
+	setConfigDirectory := flag.String("config-dir", "", "Set cordless' application directory")
 	flag.Parse()
 
 	if showShortcutsDialog != nil && *showShortcutsDialog {
@@ -20,6 +21,9 @@ func main() {
 	} else if showVersion != nil && *showVersion {
 		fmt.Printf("You are running cordless version %s\nKeep in mind that this version might not be correct for manually built versions, as those can contain additional commits.\n", version.Version)
 	} else {
+		if setConfigDirectory != nil && *setConfigDirectory != "" {
+			
+		}
 		app.Run()
 	}
 }
