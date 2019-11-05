02c54dff905027b8313d72bafbaf9bf7c65339f6
Aidan Culley <legoguy253@gmail.com>
2019-11-04 19:39:13 -0600

    Update user.go
    fixed "asociated" to "associated"

--- commands/commandimpls/user.go
+++ commands/commandimpls/user.go
@@ -52,7 +52,7 @@ const (
 	[::b]-n, --name
 		change your nickname
 	[::b]-e, --email
-		change the e-mail address asociated with your account
+		change the e-mail address associated with your account
 	[::b]-a, --avatar
 		change your avatar to a new local file of yours
 	[::b]-p, --np
