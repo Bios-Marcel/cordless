90b0cf890a6977f3e52d06584cc6f664c86898fc
Marcel Schramm <marceloschr@googlemail.com>
2019-11-14 18:41:43 +0100

    Add marshallable duration

--- times/times.go
+++ times/times.go
@@ -1,6 +1,8 @@
 package times
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"time"
 
@@ -31,4 +33,31 @@ func TimeToString(time *time.Time) string {
 // of the year.
 func AreDatesTheSameDay(t1, t2 time.Time) bool {
 	return t1.Year() == t2.Year() && t1.YearDay() == t2.YearDay()
+}
+
+type Duration time.Duration
+
+func (d Duration) MarshalJSON() ([]byte, error) {
+	return json.Marshal(time.Duration(d).String())
+}
+
+func (d *Duration) UnmarshalJSON(b []byte) error {
+	var v interface{}
+	if err := json.Unmarshal(b, &v); err != nil {
+		return err
+	}
+	switch value := v.(type) {
+	case float64:
+		*d = Duration(time.Duration(value))
+		return nil
+	case string:
+		tmp, err := time.ParseDuration(value)
+		if err != nil {
+			return err
+		}
+		*d = Duration(tmp)
+		return nil
+	default:
+		return errors.New("invalid duration")
+	}
 }
