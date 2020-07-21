52608c8c6ba30e95eb820d5f4e6362cc97661373
Tobias Berger <tobi.berger13@gmail.com>
2020-07-21 12:05:50 +0200

    Use my secret 6th brain cell to configure colors correctly

--- commands/commandimpls/account.go
+++ commands/commandimpls/account.go
@@ -193,7 +193,7 @@ func (account *Account) listAccounts(writer io.Writer) {
 	fmt.Fprintln(writer, "Available accounts:")
 	for _, acc := range config.Current.Accounts {
 		if acc.Token == config.Current.Token {
-			fmt.Fprintln(writer, "  ["+tviewutil.ColorToHex(config.GetTheme().AttentionColor)+"]> "+acc.Name+"[white]")
+			fmt.Fprintln(writer, "  ["+tviewutil.ColorToHex(config.GetTheme().AttentionColor)+"]> "+acc.Name+"["+tviewutil.ColorToHex(config.GetTheme().PrimaryTextColor)+"]")
 		} else {
 			fmt.Fprintln(writer, "  * "+acc.Name)			
 		}
