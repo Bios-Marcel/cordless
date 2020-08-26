4b8747686819ce9cba360aa5822d33a05e23b5c7
Marcel Schramm <marceloschr@googlemail.com>
2020-08-26 18:55:59 +0200

    flags for setting files and dirs aren't ignored anymore when opening the shortcut dialog directly

--- main.go
+++ main.go
@@ -20,20 +20,21 @@ func main() {
 	setConfigFilePath := flag.String("config-file", "", "Sets exact path of the configuration file")
 	flag.Parse()
 
+	if setConfigDirectory != nil {
+		config.SetConfigDirectory(*setConfigDirectory)
+	}
+	if setScriptDirectory != nil {
+		config.SetScriptDirectory(*setScriptDirectory)
+	}
+	if setConfigFilePath != nil {
+		config.SetConfigFile(*setConfigFilePath)
+	}
+
 	if showShortcutsDialog != nil && *showShortcutsDialog {
 		shortcutdialog.RunShortcutsDialogStandalone()
 	} else if showVersion != nil && *showVersion {
 		fmt.Printf("You are running cordless version %s\nKeep in mind that this version might not be correct for manually built versions, as those can contain additional commits.\n", version.Version)
 	} else {
-		if setConfigDirectory != nil {
-			config.SetConfigDirectory(*setConfigDirectory)
-		}
-		if setScriptDirectory != nil {
-			config.SetScriptDirectory(*setScriptDirectory)
-		}
-		if setConfigFilePath != nil {
-			config.SetConfigFile(*setConfigFilePath)
-		}
 		app.Run()
 	}
 }
