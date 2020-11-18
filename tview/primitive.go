4c83b1448c7f30f06463a3d48d14a423ff2aa665
Marcel Schramm <marceloschr@googlemail.com>
2020-11-18 23:33:50 +0100

    Add missing docs for Get/SetParent

--- tview/primitive.go
+++ tview/primitive.go
@@ -22,8 +22,12 @@ type Primitive interface {
 	// SetRect sets a new position of the primitive.
 	SetRect(x, y, width, height int)
 
+	// SetParent defines which component this primitive is currently being
+	// treated as a child of. This should never be called manually.
 	SetParent(Primitive)
 
+	// GetParent returns the current parent or nil if the parent hasn't been
+	// set yet.
 	GetParent() Primitive
 
 	// InputHandler returns a handler which receives key events when it has focus.
